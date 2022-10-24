d6602d47f49fa93f25e15ccaf3994e31aab20da8
AvidGamerShkunk <ghostykirby@gmail.com>
2022-10-23 20:38:27 -0400

    start adding mixin

--- src/main/java/website/avid/avidsmelting/FurnaceMixin.java
+++ src/main/java/website/avid/avidsmelting/FurnaceMixin.java
@@ -1,5 +1,11 @@
 package website.avid.avidsmelting;
 
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.client.gui.screens.inventory.FurnaceScreen;
+
+@Mixin(FurnaceScreen.class)
 public class FurnaceMixin {
 
+    public void on
 }
